2b4d0d89f289901b577c0c7c70ccd32e04b5807a
youth1n <kys010306@u.sogang.ac.kr>
2023-04-14 20:58:44 +0900

    yeonu

---
+++ Makefile
@@ -0,0 +1,2 @@
+main : main.cpp yeonu.cpp choco.cpp
+	g++ -o main main.o yeonu.o choco.o
---
+++ yeonu.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+void yeonuFunc()
+{
+    cout << "This is yeonu." << endl;
+}
