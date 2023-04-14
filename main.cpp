affd886c44e46f24def7f994e21edc56768dd3bf
youth1n <kys010306@u.sogang.ac.kr>
2023-04-14 20:33:34 +0900

    master

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void yushinFunc();
+void yeonuFunc();
+void chocoFunc();
+
+int main(int argc, char* argv[])
+{
+    cout << "yushinFunc(): ";
+    yushinFunc();
+
+    cout << "yeonuFunc(): ";
+    yeonuFunc();
+
+    cout << "chocoFunc(): ";
+    chocoFunc();
+
+    return 0;
+}
